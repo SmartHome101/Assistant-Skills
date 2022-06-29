e98f3bb1fa1ff3fb4af1b6704324f963efb1c400
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-06-29 13:23:36 +0200

    Sending a request to server  in order to access a prediction service then mapping the response to the function

---
+++ client.py
@@ -0,0 +1,40 @@
+import speech_recognition as sr
+import requests
+import json
+import time
+import Weather , News , IOT , Kitchen , Enterainment , Social , Productivity 
+from TTS import speak
+
+mapping={
+        'Greeting'      : Social.greet,
+        'Goodbye'       : Social.goodbye,
+        'Thanks'        : Social.thank,
+        'Joke'          : Enterainment.joke,
+        'PlayMusic'     : Enterainment.play_music,
+        'Cooking'       : Kitchen.cooking,
+        'Weather'       : Weather.weather,
+        'News'          : News.news,
+        'Iot'           : IOT.smartHome,
+        'Productivity'  : Productivity.productivity
+        } 
+
+while True:
+
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        recognizer.adjust_for_ambient_noise(source, duration=0.2)
+        audio = recognizer.listen(source)
+
+    # recognize speech using Google Speech Recognition
+        message = recognizer.recognize_google(audio)
+        print(message)
+
+    response = requests.post(f'http://127.0.0.1:8000/predict?message={message}').json()
+    speak(str(mapping[response['Intent']](response)))
+    time.sleep(5)
+
+    if response['Intent'] in ["Goodbye" , "Thanks"]:
+                break
+
+    time.sleep(10)
