e0919d5f63a926e3d50c6de8b04ea8dd19736988
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-07-01 02:01:37 +0200

    Finding the weather for your desired time, location, etc.

--- Weather.py
+++ Weather.py
@@ -2,36 +2,37 @@ import requests
 from TTS import speak
 
 api_key = "661d81f3b17f7764fba64a3b2e0118db"
-
+ 
 def weather(result):
     # Check if the user has provided a city name or not
     if 'location' in result['Entities']:
-        city_name = result['Entities']['location']
+        city = result['Entities']['location']
     else:
-        city_name = 'cairo'
-    
-    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city_name,api_key)
-    response = requests.get(url)
+        city = 'cairo'
 
+    url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city,api_key)
+    response = requests.get(url)
     # get information from the response object and convert it to json format to be able to access the data
     x = response.json()
+    
     if x["cod"] != "404":
         y = x["main"]
         z = x["weather"]
         # get the temperature in kelvin and convert it to celsius and weather description    
-        min_temp = round(y["temp_min"] - 273.15,2)
-        max_temp = round(y["temp_max"] - 273.15,2)
+        max_temp = int(y["temp_max"] - 273.15)
         weather_describe = z[0]["description"]
         
         if 'weather_descriptor' in result['Entities']:
             description = result['Entities']['weather_descriptor']
-            if description == weather_describe :
-                speak("Yes, weather is {}".format(weather_describe))
+            if description in weather_describe : 
+                speak(f"Yes, weather is {weather_describe}")
             else :
-                speak("No, weather is {}".format(weather_describe))
+                speak(f"No, weather is {weather_describe}")
         else :
-            res = "The weather forecast in {} is {} with a minimum of {} Celsius and a maximum of {} Celsius".format(city_name,weather_describe,min_temp,max_temp)
+            res = f"The weather forecast in {city} is {weather_describe} with a maximum of {max_temp} Celsius"
             speak(res)
+            print(res)
+            
         
     else:
         speak("There are error")
