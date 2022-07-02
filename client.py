365111074dabbece09eae7b5c64aeb1ed4d6ede0
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-07-02 05:25:29 +0200

    Add Wake Word "Leon"

--- client.py
+++ client.py
@@ -20,25 +20,39 @@ mapping={
         } 
 
 while True:
+    # obtain audio from the microphone
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
-        speak("Hi, I am Lemma personal assistant. How can I help you?")
-        recognizer.adjust_for_ambient_noise(source, duration=0.2)
-        audio = recognizer.listen(source)
-        # recognize speech using Google Speech Recognition
-        try:
+        recognizer.adjust_for_ambient_noise(source)
+        try :
+            # recognize speech 
+            recognizer.pause_threshold = .8
+            audio = recognizer.listen(source)
             message = recognizer.recognize_google(audio)
             print(message)
-            # make a request to the server and get the response in json format then mapping the response to the function
-            response = requests.post(f'http://nlp.techome.systems/predict?message={message}').json()
-            if response['Intent'] in ['Cooking','News']:
-                speak(str(mapping[response['Intent']](response)))
-                time.sleep(10)
+            # wake up the assistant
+            if 'leon' in message.lower():  
+                speak("Hi, How can I help you?")
+                recognizer = sr.Recognizer()
+                with sr.Microphone() as source:
+                    recognizer.adjust_for_ambient_noise(source)
+                    try:
+                        recognizer.pause_threshold = .8
+                        audio = recognizer.listen(source)
+                        message = recognizer.recognize_google(audio)
+                        print(message)
+                        #send a request to the server
+                        response = requests.post(f'http://nlp.techome.systems/predict?message={message}').json()
+                        if response['Intent'] in ['Cooking','News']:
+                            speak(str(mapping[response['Intent']](response)))
+                            time.sleep(10)
+                        else:
+                            speak(str(mapping[response['Intent']](response)))
+                            time.sleep(3)
+                    except:
+                        pass
             else:
-                speak(str(mapping[response['Intent']](response)))
-                time.sleep(3)
+                continue 
         except:
-            print("sorry, could not recognise try again")
-            # try "continue"
-    if response['Intent'] in ["Goodbye" , "Thanks"]:
-                break
+            continue
+    
