e2cdc07a2b05fe85881a0128f766cea8e4343554
Mohamed Nabil <67477345+MohamedNabill7@users.noreply.github.com>
2022-07-06 01:55:16 +0200

    Add a speech model

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import requests
 import re
+import os
 import json
 import time
 import Weather , News , IOT , Kitchen , Enterainment , Social , Productivity 
@@ -20,6 +21,20 @@ mapping={
         'Reminder'      : Productivity.productivity
         } 
 
+URL = 'http://sr.techome.systems/predict'
+TEMP_FILE = 'temp.wav'
+
+def save_wave_file(audio):
+    with open(TEMP_FILE, "wb") as f:
+        f.write(audio.get_wav_data())
+
+def recognize():
+    with open(TEMP_FILE, 'rb') as file:
+        files = {'file': file}
+        response = requests.post(URL, files=files)
+
+    return response.text.lower()
+
 while True:
     recognizer = sr.Recognizer()
     with sr.Microphone() as source:
@@ -27,14 +42,12 @@ while True:
         try :
             recognizer.pause_threshold = .8
             audio = recognizer.listen(source)
-            sentence = recognizer.recognize_google(audio)
-            sentence = sentence.lower()
+            sentence = recognize()
             print(sentence)
             if 'leo' in sentence:  
                 message = re.match('(.*)leo(.*)',sentence).group(2)
                 message = message.strip()
                 response = requests.post(f'http://nlp.techome.systems/predict?message={message}').json()
-                print(response)
                 if response['Intent'] in ['Cooking','News']:
                         speak(str(mapping[response['Intent']](response)))
                         time.sleep(10)
@@ -46,4 +59,6 @@ while True:
                 continue 
         except:
             continue
+        
+    os.remove(TEMP_FILE)   
     
