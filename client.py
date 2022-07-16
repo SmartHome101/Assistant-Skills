48871045d45b225d3b931397642780ac80a62db4
MahmoudAbdullah99 <mahmoudabdullah2079@gmail.com>
2022-07-16 04:01:21 +0200

    Updated speak functionality and its delay time

--- client.py
+++ client.py
@@ -6,6 +6,8 @@ import json
 import time
 import Weather , News , IOT , Kitchen , Entertainment , Social , Productivity 
 from TTS import speak
+import pygame as pg
+
 
 mapping={
         'Greeting'      : Social.Social,
@@ -21,7 +23,7 @@ mapping={
         'Reminder'      : Productivity.productivity
         } 
 
-URL = 'http://sr.techome.systems/predict'
+SR_URL = 'http://sr.techome.systems/predict'
 TEMP_FILE = 'temp.wav'
 
 def save_wave_file(audio):
@@ -31,34 +33,36 @@ def save_wave_file(audio):
 def recognize():
     with open(TEMP_FILE, 'rb') as file:
         files = {'file': file}
-        response = requests.post(URL, files=files)
+        print('will sent a request to sr')
+        response = requests.post(SR_URL, files=files)
 
     return response.text.lower()
 
 while True:
     recognizer = sr.Recognizer()
+    
     with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source)
         try :
             recognizer.pause_threshold = .8
+            print("Hey, How can I help you!")
             audio = recognizer.listen(source)
+            save_wave_file(audio)
+            print('Got the audio and will process')
             sentence = recognize()
             print(sentence)
-            if 'leo' in sentence:  
+            if 'leo' in sentence:
                 message = re.match('(.*)leo(.*)',sentence).group(2)
                 message = message.strip()
+                print(f'will sent request "{message}" to nlb')
                 response = requests.post(f'http://nlp.techome.systems/predict?message={message}').json()
-                if response['Intent'] in ['Cooking','News']:
-                        speak(str(mapping[response['Intent']](response)))
-                        time.sleep(10)
-                else:
-                    speak(str(mapping[response['Intent']](response)))
-                    time.sleep(3)
+                print(response)
+                mapping[response['Intent']](response)
                     
             else:
                 continue 
         except:
             continue
-        
+            
     os.remove(TEMP_FILE)   
     
